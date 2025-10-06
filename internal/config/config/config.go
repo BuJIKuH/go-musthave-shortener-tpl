73342cc6eba80492c52c4076ccd6db9b0c5534ca
Maksim Sandalov <santazlat@yandex.ru>
2025-10-06 18:54:33 +0300

    [Incr 4] 1. Правка config.go

--- internal/config/config/config.go
+++ internal/config/config/config.go
@@ -18,7 +18,7 @@ func InitConfig() *Config {
 	var cfg Config
 
 	flag.StringVar(&cfg.Address, "a", "localhost:8080", "Address to listen on")
-	flag.StringVar(&cfg.ShortenAddress, "b", "localhost:8000", "ShortAddress to listen on")
+	flag.StringVar(&cfg.ShortenAddress, "b", "localhost:8080", "ShortAddress to listen on")
 
 	flag.Parse()
 
