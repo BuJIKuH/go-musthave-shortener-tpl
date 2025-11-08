008a9d7ee1710b93745fe062eca96e2b8ab9b06d
Maksim Sandalov <santazlat@yandex.ru>
2025-11-08 14:03:36 +0300

    [Incr 10] 1. Добавлено подключение к БД. 2. Добавлено сохранение в бд. 3. Реализован хендрер ping.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.7
 require (
 	github.com/gin-gonic/gin v1.11.0
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.11.1
 	go.uber.org/fx v1.24.0
 	go.uber.org/zap v1.27.0
@@ -24,6 +25,7 @@ require (
 	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
@@ -46,5 +48,6 @@ require (
 	golang.org/x/text v0.27.0 // indirect
 	golang.org/x/tools v0.34.0 // indirect
 	google.golang.org/protobuf v1.36.9 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/storage/dbStorage.go
@@ -0,0 +1,71 @@
+package storage
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq"
+	"go.uber.org/zap"
+)
+
+type DBStorage struct {
+	db     *sql.DB
+	logger *zap.Logger
+}
+
+func NewDBStorage(dsn string, logger *zap.Logger) (*DBStorage, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open DB: %w", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("cannot connect to DB: %w", err)
+	}
+
+	logger.Info("Connected to PostgreSQL")
+
+	return &DBStorage{
+		db:     db,
+		logger: logger,
+	}, nil
+}
+
+func (s *DBStorage) Save(id, url string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	query := `INSERT INTO urls (short_url, original_url) VALUES ($1, $2)`
+	_, err := s.db.ExecContext(ctx, query, id, url)
+	if err != nil {
+		s.logger.Error("Failed to save record to DB", zap.Error(err))
+		return
+	}
+	s.logger.Info("Saved record", zap.String("short", id), zap.String("url", url))
+}
+
+func (s *DBStorage) Get(id string) (string, bool) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	query := `SELECT original_url FROM urls WHERE short_url = $1`
+	var original string
+	err := s.db.QueryRowContext(ctx, query, id).Scan(&original)
+	if errors.Is(sql.ErrNoRows, err) {
+		return "", false
+	}
+	if err != nil {
+		s.logger.Error("Failed to get record from DB", zap.Error(err))
+		return "", false
+	}
+	return original, true
+}
+
+func (s *DBStorage) Ping(ctx context.Context) error {
+	return s.db.PingContext(ctx)
+}
+
+func (s *DBStorage) Close() error {
+	return s.db.Close()
+}
